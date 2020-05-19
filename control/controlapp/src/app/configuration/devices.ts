63540c5a37682a6683a9c6530614de61aed84cb7
Mangar <Mangar@I3T1>
2020-05-19 07:29:25 +0200

    prepared device.ts to be json

--- control/controlapp/src/app/configuration/devices.ts
+++ control/controlapp/src/app/configuration/devices.ts
@@ -1,22 +1,7 @@
 import { pictures } from '../pictures/pictures'
 
-const deviceOnOff = {
-    actions: ['on', 'off']
-}
-
-const light = {
-    ...deviceOnOff,
-    properties: ['control'],
-    pictures: pictures.light
-}
-
-const measured =  {
-    properties: ['measured']
-}
-
 export const devices: any = [
     {
-        ...deviceOnOff,
         name: 'Main status',
         topic: '$SYS/presence',
         value: 'on',
@@ -25,14 +10,13 @@ export const devices: any = [
         pictures: pictures.house
     },
     {
-        ...deviceOnOff,
         name: 'Computer Volker power',
         topic: 'first/study/zwave/switch/master',
         properties: ['favorit', 'control'],
+        actions: ['on', 'off'],
         pictures: pictures.powerSwitch
     },
     { 
-        ...deviceOnOff,
         name: 'Backup computer',
         topic: 'ground/wardrobe/i2c/switch/backup',
         actions: ['on', 'hibernate', 'off'],
@@ -40,61 +24,62 @@ export const devices: any = [
         pictures: pictures.backup
     },
     { 
-        ...deviceOnOff,
         name: 'Charge',
         topic: 'ground/wardrobe/i2c/switch/charge',
         properties: ['control'],        
+        actions: ['on', 'off'],
         pictures: pictures.charge
     },    
     { 
-        ...deviceOnOff,
         name: 'Monitor',
         topic: 'ground/wardrobe/i2c/switch/monitor',
         properties: ['favorit', 'control'],
+        actions: ['on', 'off'],
         pictures: pictures.monitor
     },    
     { 
-        ...deviceOnOff,
         name: 'Floor heating',
         topic: 'ground/wardrobe/fs20/switch/floor heating',
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.floorHeating
     },    
     { 
-        ...deviceOnOff,
         name: 'Network switch',
         topic: 'ground/wardrobe/i2c/switch/network switch',
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.networkSwitch
     },    
     { 
-        ...deviceOnOff,
         name: 'Security camera',
         topic: 'ground/livingroom/zwave/switch/camera',
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.securityCamera
     },    
     { 
-        ...deviceOnOff,
         name: 'Electric iron',
         topic: 'first/dressingroom/zwave/switch/electric iron',
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.electricIron
     },
     {
-        ...deviceOnOff,
         name: "Battery charging",
         topic: "ground/livingroom/zwave/switch/charge",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.charge
     },
     {
-        ...light,
         name: "Ceiling floodlight",
         topic: "ground/livingroom/zwave/switch/floodlight",
+        properties: ['control'],
+        actions: ['on', 'off'],
+        pictures: pictures.light
     },
     {
-        ...deviceOnOff,
         name: "Roller shutter south-east",
         topic: "ground/livingroom/zwave/shutter/southeast",
         actions: ['up', 'down', 'stop'],
@@ -102,7 +87,6 @@ export const devices: any = [
         pictures: pictures.roller
     },
     {
-        ...deviceOnOff,
         name: "Roller shutter south-west",
         topic: "ground/livingroom/zwave/shutter/southwest",
         actions: ['on', 'off', 'stop'],
@@ -110,62 +94,61 @@ export const devices: any = [
         pictures: pictures.roller
     },
     {
-        ...deviceOnOff,
         name: "Master switch multimedia",
         topic: "ground/livingroom/zwave/switch/tvmaster",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.multimedia
     },
     {
-        ...deviceOnOff,
         name: "Multimedia amplifier",
         topic: "ground/livingroom/fs20/switch/amplifier",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.amplifier
     },
     {
-        ...deviceOnOff,
         name: "Apple TV",
         topic: "ground/livingroom/fs20/switch/appletv",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.computer
     },
     {
-        ...deviceOnOff,
         name: "Dishwasher",
         topic: "ground/kitchen/zwave/switch/dishwasher",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.dishwasher
     },
     {
-        ...deviceOnOff,
         topic: "cellar/boilerroom/zwave/switch/washing machine",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.washingMachine
     },
     {
-        ...deviceOnOff,
         name: "Refrigerator",
         topic: "ground/kitchen/zwave/switch/fridge",
         properties: ['control', 'protect'],
+        actions: ['on', 'off'],
         pictures: pictures.refrigerator
     },
     {
-        ...deviceOnOff,
         name: "Ventilation system",
         topic: "ground/wardrobe/zwave/switch/ventilation",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.ventilationSystem
     },
     {
-        ...deviceOnOff,
         name: "Smarthome Power",
         topic: "ground/wardrobe/zwave/switch/smarthome",
         properties: ['control', 'protect'],
+        actions: ['on', 'off'],
         pictures: pictures.powerSwitch
     },
     {
-        ...deviceOnOff,
         name: "Smarthome server",
         topic: "ground/wardrobe/i2c/switch/server",
         actions: ['shutdown', 'on', 'off'],
@@ -173,84 +156,83 @@ export const devices: any = [
         pictures: pictures.smarthomeServer
     },
     {
-        ...deviceOnOff,
         name: "Internet router (fritz box)",
         topic: "ground/wardrobe/i2c/switch/internet",
         properties: ['control', 'protect'],
+        actions: ['on', 'off'],
         pictures: pictures.internet
     },
     {
-        ...deviceOnOff,
         name: "Socket at parkingplace",
         topic: "outdoor/garden/main/switch/socket parkingplace",
         properties: ['control', 'notify'],
+        actions: ['on', 'off'],
         pictures: pictures.socket
     },   
     {
-        ...deviceOnOff,
         name: "Socket at stonefield",
         topic: "outdoor/garden/main/switch/socket stonefield",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.socket
     },   
     {
-        ...deviceOnOff,
         name: "Pump in cistern",
         topic: "outdoor/garden/main/switch/pump",
         properties: ['control', 'notify'],
+        actions: ['on', 'off'],
         pictures: pictures.water
     },            
     {
-        ...deviceOnOff,
         name: "Light at stairs",
         topic: "outdoor/garden/main/switch/light stairs",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.lightStairs
     },           
     {
-        ...deviceOnOff,
         name: "Lights on stonefield",
         topic: "outdoor/garden/fs20/switch/light stonefield",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.lightStonefield
     },
     {
-        ...deviceOnOff,
         name: "Lights on pathway",
         topic: "outdoor/garden/fs20/switch/light pathway",
+        actions: ['on', 'off'],
         properties: ['control'],
         pictures: pictures.lightPathway
     },
     {
-        ...deviceOnOff,
         name: "Laser printer",
         topic: "first/study/zwave/switch/laserprinter",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.printer
     },
     {
-        ...deviceOnOff,
         name: "Regina's computer",
         topic: "first/study/zwave/switch/pcregina",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.computerTower
     },
     {
-        ...deviceOnOff,
         name: "Volker's computer",
         topic: "first/study/zwave/switch/pcvolker",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.computerTower
     },
     {
-        ...deviceOnOff,
         name: "Camera boilerroom",
         topic: "cellar/boilerroom/switches/switch/camera",
         properties: ['control'],
+        actions: ['on', 'off'],
         pictures: pictures.securityCamera
     },
     {
-        ...deviceOnOff,
         name: "Roller shutter [2]",
         topic: "%/%/%/roller shutter/roller shutter key",
         actions: ['up', 'down'],
@@ -258,99 +240,92 @@ export const devices: any = [
         pictures: pictures.roller
     },
     {
-        ...light,
         name: "light [2]",
         topic: "%/%/main/light/light on time",
+        properties: ['control'],
+        actions: ['on', 'off'],
+        pictures: pictures.light
+
     },
     {
-        ...light,
         name: "light [2] [3]",
-        topic: "cellar/%/%/light/light on time",
+        topic: "cellar/hobbycellar/%/light/light on time",
+        properties: ['control'],
+        actions: ['on', 'off'],
+        pictures: pictures.light
     },
     {
-        ...measured,
         name: "temperature [2]",
         topic: "%/%/%/temperature and humidity sensor/temperature in celsius",
+        properties: ['measured'],
         pictures: pictures.temperature
     },
     {
-        ...measured,
         name: "temperature",
         topic: "%/%/%/%/temperature",
+        properties: ['measured'],
         pictures: pictures.temperature
     },
     {
-        ...measured,
         name: "internal temperature",
         topic: "%/%/%/%/internal temperature in celsius",
+        properties: ['measured'],
         pictures: pictures.temperature
     },
     {
-        topic: "ground/hallway/center/temperature and humidity sensor/temperature in celsius",
-        properties: ['favorit', 'measured'],
-    },
-    {
-        ...measured,
         name: "humidity [2]",
         topic: "%/%/%/temperature and humidity sensor/humidity in percent",
+        properties: ['measured'],
         pictures: pictures.humidity
     },
     {
-        ...measured,
         name: "humidity",
         topic: "%/%/%/%/humidity",
+        properties: ['measured'],
         pictures: pictures.humidity
     },
     {
-        topic: "ground/hallway/center/temperature and humidity sensor/humidity in percent",
-        properties: ['favorit', 'measured'],
-    },
-    {
-        ...measured,
         name: "outside temperature",
         topic: "outdoor/garden/main/weather/temperature",
         properties: ['measured', 'favorit'],
         pictures: pictures.temperature
     },
     {
-        ...measured,
         topic: "%/%/%/%/pressure",
+        properties: ['measured'],
         pictures: pictures.pressure
     },
     {
-        ...measured,
         name: "pressure",
         topic: "outdoor/garden/main/weather/pressure",
+        properties: ['measured'],
         pictures: pictures.pressure
     },
     {
-        ...measured,
         name: "outside humidity",
         topic: "outdoor/garden/main/weather/humidity",
+        properties: ['measured'],
         pictures: pictures.humidity
     },
     {
-        ...measured,
         name: "power consumption",
         topic: "solar/consumption",
         properties: ['measured', 'favorit'],
         pictures: pictures.consumption
     },
     {
-        ...measured,
         name: "power feed in",
         topic: "solar/feedin",
-        properties: ['measured', 'favorit'],
+        properties: ['measured'],
         pictures: pictures.feedin
     },
     {
-        ...measured,
         name: "average power feed in",
         topic: "solar/average feedin",
+        properties: ['measured'],
         pictures: pictures.feedin
     },
     {
-        ...measured,
         name: "solar power yield",
         topic: "solar/solar yield",
         properties: ['measured', 'favorit'],
